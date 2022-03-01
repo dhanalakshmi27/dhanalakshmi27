1c4dc1d370928d1629432bc1866f028bf17a20a9
dhanalakshmi27 <dhanapalampathi5@gmail.com>
2022-02-28 20:36:20 -0800

    Assignment Commit

--- src/main/java/com/assignment/repo/LoginRepository.java
+++ src/main/java/com/assignment/repo/LoginRepository.java
@@ -2,5 +2,7 @@ package com.assignment.repo;
 
 
 public interface LoginRepository extends CrudRepository<LoginEntity,String>{
+
+    LoginEntity findOneByUserIdAndPassword(String userId, String password);
 	
 }
