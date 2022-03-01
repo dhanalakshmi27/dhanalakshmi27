37bd053ae0428314d707c59b2bcf2238f80b9448
dhanalakshmi27 <dhanapalampathi5@gmail.com>
2022-02-28 20:49:00 -0800

    Assignment Commit

--- src/main/java/com/assignment/repo/LoginRepository.java
+++ src/main/java/com/assignment/repo/LoginRepository.java
@@ -1,8 +1,12 @@
 package com.assignment.repo;
 
+import com.assignment.entity.LoginEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LoginRepository extends CrudRepository<LoginEntity,String>{
-
-    LoginEntity findOneByUserIdAndPassword(String userId, String password);
+	
+	LoginEntity findOneByUserIdAndPassword(String userId, String password);
 	
 }
